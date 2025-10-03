a903371cfe42853bcc284631ddc46158e090d1a9
Geovanni Colon <vanni2249@gmail.com>
2025-10-03 00:25:02 -0400

    Update User Item Show UI

--- resources/views/livewire/users/items/index.blade.php
+++ resources/views/livewire/users/items/index.blade.php
@@ -1,21 +1,44 @@
 <div>
     <div class="space-y-4">
+        <!-- Header -->
         <x-card>
-            Results for: <strong>{{ $items->total() }}</strong> items found.
-
-            @if ($search)
-                <div class="mt-2">
-                    You searched for: <strong>"{{ $search }}"</strong>
+            <div class="flex justify-between items-center">
+                <div>
+                    @if ($search)
+                        <div class="text-gray-800 text-sm">
+                            <span class="hidden lg:inline">
+                                You searched for: 
+                            </span>
+                            <span class="lg:hidden">
+                                Searched:
+                            </span>
+                            <strong class="text-gray-950">"{{ $search }}"</strong>
+                        </div>
+                    @endif
+                    <div class="text-gray-800 text-sm">
+                        Results for: <strong>{{ $items->total() }}</strong> items found.
+                    </div>
                 </div>
-            @else
-                <div class="mt-2 text-gray-500">
-                    You can search for items using the search bar above.
+                <div class="flex items-center space-x-2">
+                    <div class="bg-gray-200 rounded-md p-1 items-center">
+                        <span class="pl-2 uppercase text-xs font-bold text-gray-600 leading-tight">Show</span>
+                        <select wire:model.live="perPage" class="mx-2 rounded-md text-sm">
+                            <option value="10">10</option>
+                            <option value="20">20</option>
+                            <option value="30">30</option>
+                            <option value="40">40</option>
+                        </select>
+                    </div>
+                    <div class="lg:hidden">
+                        <x-icon-button icon="filter" @click="$dispatch('open-modal', 'mobile-filters')"/>
+                    </div>
                 </div>
-            @endif
+            </div>
         </x-card>
+        <!-- Body -->
         <div class="grid grid-cols-12 gap-2 md:gap-4">
             <!-- Filter -->
-            <div class="col-span-full lg:col-span-3">
+            <div class="col-span-full lg:col-span-3 hidden lg:block">
                 <x-card></x-card>
             </div>
             <!-- Results -->
@@ -27,12 +50,16 @@
                             <x-item href="{{ route('items.show', $item) }}" :item="$item" wire:navigate />
                         </div>
                     @endforeach
-                </di    v>
+                </di v>
             </div>
         </div>
+        <!-- Pagination -->
         <x-card class="col-span-full">
-
             {{ $items->links() }}
         </x-card>
     </div>
+
+    <!-- Mobile modal -->
+    <x-modal name="mobile-filters" title="Filters" size="sm">
+    </x-modal>
 </div>
