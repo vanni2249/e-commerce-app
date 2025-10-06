167c5bb0733819845fdb5d26bb774cdc7f1f736f
Geovanni Colon Barrios <vanni2249@gmail.com>
2025-10-06 13:33:36 -0400

    Update User Welcome Page

--- resources/views/livewire/users/welcome/index.blade.php
+++ resources/views/livewire/users/welcome/index.blade.php
@@ -149,12 +149,12 @@
             @for ($i = 0; $i < 5; $i++)
                 <div class="flex-shrink-0 space-y-4 bg-white lg:flex-shrink-1 w-4/5 md:w-2/5 lg:w-1/2 rounded-xl p-4">
                     <header class="flex justify-between items-center">
-                        <h2 class="font-bold text-sm text-gray-700">
+                        <h2 class="font-bold text-lg text-gray-800">
                             Suppliers' Picks
                         </h2>
-                        <a href="#" class="text-blue-800 text-xs font-bold ml-4">Details</a>
+                        {{-- <a href="#" class="text-blue-800 text-sm font-bold ml-4">Details</a> --}}
                     </header>
-                    <p class="text-xs text-gray-600">
+                    <p class="text-sm text-gray-600">
                         Lorem ipsum dolor sit amet ison, consectetur adipiscing elit.
                     </p>
                     <div class="grid grid-cols-2 gap-2">
@@ -165,6 +165,11 @@
                             </div>
                         @endfor
                     </div>
+                    <footer class="mt-4">
+                        <button class="w-full bg-blue-800 text-white text-sm p-2 rounded hover:bg-blue-900 transition">
+                            Shop Now
+                        </button>
+                    </footer>
                 </div>
             @endfor
 
