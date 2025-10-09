979575c7e8e4f57060e5c533f77d7ac3ec6607c6
Geovanni Colon Barrios <vanni2249@gmail.com>
2025-10-09 10:24:33 -0400

    Update Seller Layout

--- resources/views/components/layouts/seller.blade.php
+++ resources/views/components/layouts/seller.blade.php
@@ -20,7 +20,7 @@
         @livewire('sellers.layout.sidebar')
     </div>
 
-    <div id="main-content" class="flex-grow flex ml-64 transition-all flex-col">
+    <div id="main-content" class="flex-grow flex lg:ml-64 transition-all flex-col">
         @livewire('sellers.layout.navbar')
         <main class="flex-grow min-h-96 p-4">
             {{ $slot }}
@@ -40,6 +40,33 @@
     </div>
     @stack('scripts')
     @livewireScripts
+    <script>
+        const sidebar = document.getElementById('sidebar');
+        const mainContent = document.getElementById('main-content');
+        const toggleButton = document.getElementById('toggleButton');
+        const closeButtonMobile = document.getElementById('closeButtonMobile');
+
+        toggleButton.addEventListener('click', function() {
+            // Check if is lg screen
+            if (window.innerWidth >= 1024) {
+                sidebar.classList.toggle('lg:w-64');
+                mainContent.classList.toggle('lg:ml-64');
+            } else {
+                sidebar.classList.toggle('w-0');
+                sidebar.classList.toggle('w-64');
+                mainContent.classList.toggle('lg:ml-0');
+            }
+        });
+
+        closeButtonMobile.addEventListener('click', function() {
+            if (window.innerWidth < 1024) {
+                sidebar.classList.add('w-0');
+                sidebar.classList.remove('w-64');
+                mainContent.classList.add('lg:ml-0');
+                mainContent.classList.remove('lg:ml-64');
+            }
+        });
+    </script>
 </body>
 
 </html>
