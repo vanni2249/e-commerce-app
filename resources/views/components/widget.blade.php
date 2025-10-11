04a0aeebbda70011475fafd9ed49df730385ecf6
Geovanni Colon Barrios <vanni2249@gmail.com>
2025-10-11 00:09:46 -0400

    Added Component widget

--- resources/views/components/widget.blade.php
+++ resources/views/components/widget.blade.php
@@ -1,20 +1,28 @@
-@props(['color' => 'bg-white', 'title' => '', 'subtitle' => '', 'value' => '', 'border' => 'border-gray-300', 'icon' =>
-'circle', 'lineColor' => 'gray'])
-<div {{ $attributes->merge(['class' => $color . ' ' . $border . ' p-2 md:p-4 rounded border-l-2 border-'.$lineColor.'-300 flex items-center
-    justify-between']) }}>
-    <div>
-        <header class="flex justify-between items-center">
-            <h2 class="text-xs text-gray-600 leading-3 font-bold uppercase">
-                {{ $title }}
-            </h2>
-            <div>
-                {{ $subtitle }}
-            </div>
-        </header>
-        <div class="text-xl text-gray-800 font-bold mt-2">{{ $value }}</div>
-    </div>
+@props([
+    'bg' => 'bg-white',
+    'title' => '',
+    'right' => '',
+    'value' => '',
+    'border' => 'border-gray-300',
+    'icon' => 'circle',
+    'lineColor' => 'gray',
+    'footer' => '',
+])
 
-    <div>
-        <img src="{{ asset('icons/'.$icon.'.svg') }}" class="w-8" alt="">
-    </div>
+@php
+    $classes = $bg . ' p-4 rounded-xl border-l-2 border-' . $lineColor . '-300 ' . $attributes->get('class', '');
+@endphp
+<div {{ $attributes->merge(['class' => $classes]) }}>
+    <header class="flex justify-between items-center">
+        <h2 class="text-sm text-gray-950 leading-3 font-bold">
+            {{ $title }}
+        </h2>
+        <div class="">
+            {{ $right?? ' ' }}
+        </div>
+    </header>
+    <div class="text-xl text-gray-950 font-bold mt-2">{{ $value }}</div>
+    <footer class="">
+       {{ $footer ?? '' }}
+    </footer>
 </div>
