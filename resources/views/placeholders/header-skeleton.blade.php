bfe27d79c552d8d5dc77200b6a2889b13bfd0568
Geovanni Colon <vanni2249@gmail.com>
2025-12-28 13:30:30 -0400

    feat: adding placeholders

---
+++ resources/views/placeholders/header-skeleton.blade.php
@@ -0,0 +1,16 @@
+<div class="space-y-4">
+
+    <header class="">
+        <div class="flex justify-between items-center">
+            <div class="h-4 bg-gray-300 rounded w-24"></div>
+            <div class="h-4 bg-gray-300 rounded w-20"></div>
+        </div>
+    </header>
+    <div class="flex space-x-2">
+        @for ($i = 0; $i < 2; $i++)
+            <div class="h-4 bg-gray-300 rounded w-16"></div>
+            <div class="h-4 bg-gray-300 rounded w-20"></div>
+        @endfor
+
+    </div>
+</div>
--- resources/views/placeholders/header-table-skeleton.blade.php
+++ resources/views/placeholders/header-table-skeleton.blade.php
@@ -1,6 +1,20 @@
 <div class="space-y-4">
+
+    <header class="">
+        <div class="flex justify-between items-center">
+            <div class="h-4 bg-gray-300 rounded w-24"></div>
+            <div class="h-4 bg-gray-300 rounded w-20"></div>
+        </div>
+    </header>
+    <div class="flex space-x-2">
+        @for ($i = 0; $i < 2; $i++)
+            <div class="h-4 bg-gray-300 rounded w-16"></div>
+            <div class="h-4 bg-gray-300 rounded w-20"></div>
+        @endfor
+
+    </div>
     <div class="bg-white rounded-lg shadow p-4">
-        <div class="animate-pulse space-y-4">
+        <div class="animate-pulse space-y-4 ">
             <!-- Header skeleton -->
             <div class="flex items-center justify-between">
                 <div class="h-6 bg-gray-200 rounded w-32"></div>
