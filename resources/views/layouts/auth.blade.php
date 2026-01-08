d4f198381f72d18c35899e72f17472115c290711
Geovanni Colon Barrios <vanni2249@gmail.com>
2026-01-08 14:14:44 -0400

    feat:adding layouts admin-sidebar, auth, user

---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Welcome</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+
+    <!-- Styles / Scripts -->
+    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @else
+    @endif
+    @livewireStyles
+</head>
+
+<body class="bg-gray-200 font-sans flex flex-col p-4 min-h-screen">
+    <header class="mb-4 mx-auto">
+        @if(in_array(request()->segment(1), ['login', 'register', 'password/reset']))
+                <a href="/" class="text-2xl font-bold" wire:navigate>Myapp's</a>
+        @endif
+    </header>
+    <div class="grow sm:mx-auto">
+        {{ $slot }}
+
+    </div>
+    @livewireScripts
+</body>
+
+</html>
---
+++ resources/views/layouts/user.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>Welcome</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+
+    <!-- Styles / Scripts -->
+    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @else
+    @endif
+    @livewireStyles
+</head>
+
+<body class="bg-gray-100 font-sans antialiased">
+    <!-- NavBar -->
+    <nav class="w-full sticky top-0 z-50">
+        @livewire('users.layout.navbar')
+    </nav>
+
+    <!-- Menu -->
+    @livewire('users.layout.menu')
+
+    <!-- Page Content -->
+    <div class="min-h-[756px] flex flex-col max-w-7xl mx-auto space-y-4 p-4">
+        <!-- Main -->
+        <main class="grow">
+            {{ $slot }}
+        </main>
+    </div>
+    <!-- Footer -->
+    @livewire('users.layout.footer')
+    
+    <!-- Scripts -->
+    @stack('scripts')
+    @livewireScripts
+</body>
+
+</html>
